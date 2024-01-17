8165b8bc7fb9333f37fcb3f3ef3f4e886f8f437f
Andrey Filyanin <filyaniny@gmail.com>
2024-01-17 21:03:01 +0400

    Fix some naming

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
-import { lusitiana } from './ui/fonts';
+import { lusitana } from './ui/fonts';
 import Image from 'next/image';
 
 
@@ -15,7 +15,7 @@ export default function Page() {
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <div className={styles.shape} />
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitiana.className} antilaiased`}>
+          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className} antilaiased`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,4 +2,4 @@ import { Inter } from 'next/font/google';
 import { Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
-export const lusitiana = Lusitana({ weight: ['400', '700'], subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: ['400', '700'], subsets: ['latin'] });
